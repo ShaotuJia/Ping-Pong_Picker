2739f455cc3714da477641a127dfffc9a689a7be
ShaotuJia <shaotu.jia@hotmail.com>
2017-12-10 05:41:12 -0500

    #Defect 2; problem: cannot LookupTransform(/base_footprint, odom); solution: add function waitForTransform

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,25 @@
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "walk_turtlebot");	///< Initialize node with name "walk_turtlebot"
 	Walk walker;	///< declare a object for class Walk
-	walker.set_initial_pose(-1, -1);	///< set up initial pose for turtlebot
-	walker.set_forward(0.2);	///< set up linear velocity when moving forward
-	walker.set_turn(-6.28);	///< set up angular velocity when hitting obstacles
+	walker.set_initial_pose(0, 0);	///< set up initial pose for turtlebot
+	walker.set_linear(0.4);	///< set up linear velocity when moving forward
+	walker.set_angular(1);	///< set up angular velocity when hitting obstacles
 	walker.set_up_position();	///< locate turtlebot in desired location
-	walker.move();	///< move the turtlebot with a simple walk algorithm like roomba
+	//walker.set_up_worktime(1);	///< set up the work time limit
+	//walker.linear_move(10);	///< move the turtlebot with a simple walk algorithm like roomba
+	//walker.rotate(3.5);
+	//walker.where_turtle();
+
+	tf::TransformListener listener;
+	ros::Rate rate(10);
+	while(ros::ok()) {
+		tf::StampedTransform transform;
+	    listener.waitForTransform("/base_footprint", "/odom",ros::Time(0), ros::Duration(10.0));
+		listener.lookupTransform("/base_footprint","/odom",ros::Time(0),transform);
+		ROS_INFO("get tf %f",transform.getOrigin().x());
+		rate.sleep();
+	}
+
 
 }
 
